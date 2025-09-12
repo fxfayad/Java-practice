7a6b87e6ce80c5878d5170dbaee622630872c2c7
fxfayad <fxfayad@gmail.com>
2025-09-12 23:02:47 +0600

    practice

---
+++ Practice/src/Method/Person.java
@@ -0,0 +1,11 @@
+package Method;
+
+public class Person {
+    String name;
+    int id;
+
+void displayinformation(){
+    System.out.println("Name: "+name);
+    System.out.println("ID: "+id);
+}
+}
