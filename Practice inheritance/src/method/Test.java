5a41c3797cbf17bf9ab4387115f0dc93e218160e
fxfayad <fxfayad@gmail.com>
2025-09-12 22:34:56 +0600

    Practice

---
+++ Practice inheritance/src/method/Test.java
@@ -0,0 +1,11 @@
+package method;
+
+public class Test {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.name = "Fayad";
+        person.id = 1071;
+
+    person.displayinformation();
+    }
+}
