7ff44309da0a7c0a6e62f3286ed599354a3cfb08
fxfayad <fxfayad@gmail.com>
2025-09-12 23:14:01 +0600

    add

---
+++ Practice/src/Method/Test2.java
@@ -0,0 +1,11 @@
+package Method;
+
+public class Test2 {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.name = "Fayad";
+        person.id = 1071;
+
+    person.displayinformation();
+    }
+}
