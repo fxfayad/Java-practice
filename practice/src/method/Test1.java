ad924946de438e5b5a63688f1d6133fd42ecb788
fxfayad <fxfayad@gmail.com>
2025-09-12 22:45:51 +0600

    update

---
+++ practice/src/method/Test1.java
@@ -0,0 +1,10 @@
+package method;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        person1.name = "Fayad";
+        person1.id = 1071;
+    person1.displayinformation();
+    }
+}
