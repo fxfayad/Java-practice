93b96d12c673340931e72fc82b0d52290f48a475
fxfayad <fxfayad@gmail.com>
2025-10-06 21:04:58 +0600

    Update

--- CanteenManagement/src/Test.java
+++ CanteenManagement/src/Test.java
@@ -1,6 +1,6 @@
 import service.CanteenService;
 
-public class Main {
+public class Test {
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(CanteenService::new);
     }
